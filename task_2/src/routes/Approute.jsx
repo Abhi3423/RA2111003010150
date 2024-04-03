2fa9b8284176353a14ebfc9272e071084cfdd32b
Abhishek Yadav <as2165@srmist.edu.in>
2024-04-03 20:58:12 +0530

    Router fix

--- task_2/src/routes/Approute.jsx
+++ task_2/src/routes/Approute.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NoRouteFound } from "./NoRouteFound";
 import Home from "../components/Home/home";
 import Product from "../components/Product/product";
 
 const AppRoute = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product" element={<Product />} />
-
-      <Route path="*" element={<NoRouteFound />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<NoRouteFound />} />
+      </Routes>
+    </Router>
   );
 };
+
 export { AppRoute as Routes };
