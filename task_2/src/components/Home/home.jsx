e5f565d381625ab6bd89f5c3e83c75934ba88673
Abhishek Yadav <as2165@srmist.edu.in>
2024-04-03 21:35:07 +0530

    Update home.jsx

--- task_2/src/components/Home/home.jsx
+++ task_2/src/components/Home/home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import axios from 'axios'
+import {HOST} from '../../shared/const/const'
+import { useEffect } from 'react'
 
-function home() {
+function Home() {
+  useEffect(() => {
+
+    async function getStartdata() {
+      let res = await axios.get(`${HOST}/categories`)
+      console.log(res?.data)
+    }
+    getStartdata()
+
+  }, [])
+
   return (
     <div className='w-[100%] h-[100%] flex justify-center'>
       
@@ -9,4 +21,4 @@ function home() {
   )
 }
 
-export default home
+export default Home
